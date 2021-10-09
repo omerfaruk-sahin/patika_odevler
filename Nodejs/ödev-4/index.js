a491a1dd2fde2fda3364fc52d9420dbdac09382a
Ömer Faruk ŞAHİN <79583574+omerfaruk-sahin@users.noreply.github.com>
2021-10-09 19:54:18 +0300

    Add files via upload

---
+++ Nodejs/ödev-4/index.js
@@ -0,0 +1,15 @@
+const fs= require('fs');
+
+fs.writeFile('employees.json', '{"name": "Employee 1 Name", "salary": 2000}', 'utf8', (err)=> {
+    if (err) console.log(err);
+    console.log("Dosya oluşturuldu.")
+})
+fs.readFile('employees.json',(err,data)=>{
+    if (err) console.log(err);
+    console.log(JSON.parse(data))
+    console.log("Dosya okundu")
+})
+fs.appendFile('employees.json','\n{"name": "Employee 2 Name", "salary": 5000}' ,'utf8',(err)=>{
+    if (err) console.log(err);
+    console.log("Dosya güncellendi")
+})
